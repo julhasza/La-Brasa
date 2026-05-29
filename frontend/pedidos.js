228df3f573c9329502436a5be77fdb5c7559b0f5
julhasza <juliasza2402@gmail.com>
2026-05-29 18:29:41 -0300

    Update pedidos.js

--- frontend/pedidos.js
+++ frontend/pedidos.js
@@ -1,5 +1,4 @@
-const PEDIDOS_API_BASE_URL = 'http://localhost:3001/api';
-
+const PEDIDOS_API_BASE_URL = 'https://la-brasa.onrender.com/api';
 document.addEventListener('DOMContentLoaded', () => {
     const listaPedidos = document.getElementById('lista-pedidos');
     const btnAtualizar = document.getElementById('btn-atualizar');
